d868a82a0c3d9df47e098716271d4c7c4aba4473
altynbekamirzhanov <altynbekamirzhanov988@gmail.com>
2019-08-08 17:38:20 +0600

    Update third.php

--- week8/third.php
+++ week8/third.php
@@ -50,7 +50,7 @@
 		<div class="cards">
 			<?php
 				$cars = [["maker"=>"Toyota","model"=>"Carina","year"=>2001,"price"=>20000,"image"=>"https://a.d-cd.net/cea52es-480.jpg"],
-						["maker"=>"Toyota","model"=>"Camry","year"=>2005,"price"=>30000,"image"=>"https://d1hu588lul0tna.cloudfront.net/toyotaone/ruru/002-camry-exclusive-next-steps_tcm-3020-870114.jpg"],
+						["maker"=>"Toyota","model"=>"Camry","year"=>2005,"price"=>30000,"image"=>"https://t1-cms-3.images.toyota-europe.com/toyotaone/kzru/%D0%A1amry-70-2_tcm-3051-1330802.jpg"],
 						["maker"=>"Audi","model"=>"A8","year"=>1986,"price"=>12000,"image"=>"http://www.theautohost.com/_contentPages/vehicleContentPages/audi/2017/A8%20L/images/2017-Audi-A8L-exterior-grille.jpg"],
 						["maker"=>"Audi","model"=>"A6","year"=>2005,"price"=>30000,"image"=>"https://www.cstatic-images.com/car-pictures/xl/USC60AUC021B021001.png"],
 						["maker"=>"BMW","model"=>"X5","year"=>2007,"price"=>17000,"image"=>"https://www.bmw.ru/content/dam/bmw/marketRU/bmw_ru/all-models/x-series/x5/2015/at-a-glance/x5_at-a-glance_stagepresentation.jpg/_jcr_content/renditions/cq5dam.resized.img.1680.large.time1452864075065.jpg"],
